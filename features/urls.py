a27003a08d758f7d9060dc8e05e01e3e9546dcc0
Roshan Dahal <roshandahal.r@gmail.com>
2023-08-19 18:35:44 +0545

    initrialisda

---
+++ features/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.urls import re_path
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static  
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+]
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
