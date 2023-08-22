8eef6cadb934ae188e475d64b4a2f99822d2362d
Roshan Dahal <roshandahal.r@gmail.com>
2023-08-22 12:28:36 +0545

    asdsad

--- staticfiles/assets/js/main.js
+++ staticfiles/assets/js/main.js
@@ -28,7 +28,7 @@ $('.my-paroller').paroller();
 $(function() {
   $('.chart').easyPieChart({
       size: 160,
-      barColor: '#E80000',
+      barColor: '#d9b45b',
       scaleColor: false,
       lineWidth: 8,
       trackColor: '#ffffff',
